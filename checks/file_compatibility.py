dd9d8d0f05fe215dc3113f203a096c920ac7b335
Michael W <michael.weigel@br-automation.com>
2025-10-28 14:31:15 +0100

    Parallelize file reference search (#101)
    Also move the code into own methods

--- checks/file_compatibility.py
+++ checks/file_compatibility.py
@@ -7,32 +7,7 @@ from utils import utils
 version_pattern = re.compile(r'AutomationStudio (?:Working)?Version="?([\d.]+)')
 
 
-def check_file_version(file_path: Path) -> list:
-    """
-    Checks the version of a given file
-    """
-    accepted_prefixes = ("4.12", "6.")
-
-    result = set()
-    content = utils.read_file(file_path)
-    version_match = version_pattern.search(content)
-    if version_match:
-        version = version_match.group(1).strip()
-        if not version.startswith(accepted_prefixes):
-            result.add((file_path, version))
-    else:
-        result.add((file_path, "Version Unknown"))
-    return list(result)
-
-
-def check_files_for_compatibility(project_path: Path, log, verbose=False) -> None:
-    """
-    Checks the compatibility of .apj and .hw files within an apj_path.
-    Validates that files have a minimum required version.
-    Generates warning when files are converted to a new format in AS6 that may break references.
-    """
-    log("─" * 80 + "\nChecking project and hardware files for compatibility...")
-
+def check_all_file_versions(project_path: Path, log, verbose: bool) -> None:
     physical_path = project_path / "Physical"
 
     results = utils.scan_files_parallel(project_path, [".apj"], check_file_version)
@@ -54,23 +29,29 @@ def check_files_for_compatibility(project_path: Path, log, verbose=False) -> Non
         if verbose:
             log("All project and hardware files are valid.", severity="VERBOSE")
 
-    # --- Search for *.pkg files in config_folder and subfolders ---
-    reference_files = []
-    for path in physical_path.rglob("*.pkg"):
-        # Ignore files in any directory named 'mappView'
-        if "mappView" in path.parts:
-            continue
-        if path.is_file():
-            try:
-                tree = etree.parse(str(path))
-                root = tree.getroot()
-                # Suche mit XPath nach allen Elementen mit Type="File" und Reference="true"
-                matches = root.xpath('.//*[@Type="File" and @Reference="true"]')
-                if matches:
-                    reference_files.append(str(path))
-            except Exception as e:
-                # Fallback: ignore file if not valid XML
-                pass
+
+def check_file_version(file_path: Path) -> list:
+    """
+    Checks the version of a given file
+    """
+    accepted_prefixes = ("4.12", "6.")
+
+    result = set()
+    content = utils.read_file(file_path)
+    version_match = version_pattern.search(content)
+    if version_match:
+        version = version_match.group(1).strip()
+        if not version.startswith(accepted_prefixes):
+            result.add((file_path, version))
+    else:
+        result.add((file_path, "Version Unknown"))
+    return list(result)
+
+
+def check_for_referenced_files(project_path: Path, log, verbose=False) -> None:
+    reference_files = utils.scan_files_parallel(
+        project_path / "Physical", [".pkg"], has_file_reference
+    )
 
     if reference_files:
         log(
@@ -82,3 +63,36 @@ def check_files_for_compatibility(project_path: Path, log, verbose=False) -> Non
         for ref_file in reference_files:
             output += f"\n- {ref_file}"
         log(output[1:])
+
+
+def has_file_reference(file_path: Path) -> list:
+    """
+    Checks if a .pkg file contains referenced files.
+    """
+    results = []
+    if not file_path.is_file() or "mappView" in file_path.parts:
+        return results
+
+    try:
+        tree = etree.parse(str(file_path))
+        root = tree.getroot()
+        # Search with XPath for all elements with Type="File" and Reference="true"
+        matches = root.xpath('.//*[@Type="File" and @Reference="true"]')
+        if matches:
+            results.append(file_path)
+    except Exception as e:
+        # Fallback: ignore file if not valid XML
+        pass
+    return results
+
+
+def check_files_for_compatibility(project_path: Path, log, verbose=False) -> None:
+    """
+    Checks the compatibility of .apj and .hw files within an apj_path.
+    Validates that files have a minimum required version.
+    Generates warning when files are converted to a new format in AS6 that may break references.
+    """
+    log("─" * 80 + "\nChecking project and hardware files for compatibility...")
+
+    check_all_file_versions(project_path, log, verbose)
+    check_for_referenced_files(project_path, log, verbose)
